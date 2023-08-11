85c973322998563386a17008242f0f6bd9ac10cc
gilymerkado <gilymerkado@gmail.com>
2023-08-11 11:24:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMemeber() {
+  const skills = document.querySelector('.skills');
+  const skillsTop = skills.offsetTop;
+  const skillsHeight = skills.offsetHeight;
+  const skillsBottom = skillsTop + skillsHeight;
+
+  const member = document.querySelector('.member');
+  const memberTop = member.offsetTop;
+  const memberHeight = member.offsetHeight;
+  const memberBottom = memberTop + memberHeight;
+
+  if (skillsBottom > memberTop && skillsTop < memberBottom) {
+    member.classList.add('active');
+  } else {
+    member.classList.remove('active');
+  }
+}
